c4a338d276035b087ab317fbe2f708e8d73236cd
Emin <me@emin.chat>
2026-07-23 13:59:58 +0800

    feat: accept os.PathLike in py_config path bindings

--- src/interface/python/py_config/py_register_config.h
+++ src/interface/python/py_config/py_register_config.h
@@ -17,21 +17,24 @@
 #pragma once
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <pybind11/stl/filesystem.h>
 #include "py_config.h"
 namespace python_interface {
 namespace py = pybind11;
 
 void register_config(pybind11::module& m){
-    m.def("db_init", db_init,
-        py::arg("config_path") = "",
-        py::arg("tech_lef_path") = "",
-        py::arg("lef_paths") = std::vector<std::string> {},
-        py::arg("def_path") = "",
-        py::arg("verilog_path") = "",
-        py::arg("output_path") = "",
-        py::arg("feature_path") = "",
-        py::arg("lib_paths") = std::vector<std::string>{},
-        py::arg("sdc_path") = ""
+    m.def("flow_init", flow_init, py::arg("flow_config"));
+    
+    m.def("db_init", db_init, 
+        py::arg("config_path") = py::none(),
+        py::arg("tech_lef_path") = py::none(),
+        py::arg("lef_paths") = std::vector<std::filesystem::path>{},
+        py::arg("def_path") = py::none(),
+        py::arg("verilog_path") = py::none(),
+        py::arg("output_path") = py::none(),
+        py::arg("feature_path") = py::none(),
+        py::arg("lib_paths") = std::vector<std::filesystem::path>{},
+        py::arg("sdc_path") = py::none()
     );
 }
 }  // namespace python_interface
